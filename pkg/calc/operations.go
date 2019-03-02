dcc234fc04d841d0ba26d3aa63316a24ee691e36
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2019-03-02 22:37:09 +0100

    First test completed

--- pkg/calc/operations.go
+++ pkg/calc/operations.go
@@ -1,1 +1,9 @@
 package calc
+
+func Double(x int) int {
+	return 2 * x
+}
+
+func Sum(x, y int) int {
+	return 0
+}
--- pkg/calc/operations_test.go
+++ pkg/calc/operations_test.go
@@ -2,7 +2,14 @@ package calc
 
 import "testing"
 
+// * Testing double
+func TestDouble(t *testing.T) {
+	if 4 != Double(2) {
+		t.Fatal("4 should be = Double(2)")
+	}
+}
+
 // * Testing sum
 func TestSum(t *testing.T) {
-	t.Fatal("first test failed")
+	t.Fatal("Test not implemented")
 }
